88c565ef4c9c62ea9b227aa0afbfedd8435e32d2
Sz <szyen.lin@gmail.com>
2026-06-07 21:32:55 +0800

    config update

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,31 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint' // 使用這個封裝套件
+import eslintConfigPrettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules', 'coverage']),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{ts,tsx}'], // 專注在 TS/TSX 檔案
     extends: [
       js.configs.recommended,
+      ...tseslint.configs.recommended, // 展開並使用推薦的 TS 規則
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      eslintConfigPrettier,
     ],
     languageOptions: {
       globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      parser: tseslint.parser, // 指定解析器
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
   },
 ])
